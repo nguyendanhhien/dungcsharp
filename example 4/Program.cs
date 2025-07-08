6249e1593b8467233465a3ccb6b4a274b66446f3
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-08 21:15:24 +0700

    add
    add

---
+++ example 4/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int sotuoi;
+        Console.WriteLine("Hay nhap so tuoi cua ban");
+        sotuoi = Convert.ToInt32(Console.ReadLine());
+        bool dutuoi = sotuoi >= 18;
+        Console.WriteLine($"Ban da du tuoi lay chong nha {dutuoi}");
+        
+    }
+}
