dda810990d6961887ec4f667448d026d0ea93352
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-08 19:27:12 +0700

    add
    add

---
+++ example/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        DateTime dateTime = DateTime.Now;
+        Console.WriteLine($"{dateTime.Hour}{dateTime.Minute}{dateTime.Second}.");
+    }
+}
