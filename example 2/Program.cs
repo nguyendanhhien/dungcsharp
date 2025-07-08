b3cca5ebbb01b5280b82efa5fe4b144c9955d31e
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-08 19:42:06 +0700

    add
    add

---
+++ example 2/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        float chieucao;
+        float chieurong;
+        Console.WriteLine("Hay nhap chieu cao");
+        chieucao = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Hay nhap chieu rong");
+        chieurong = Convert.ToInt32(Console.ReadLine());
+        float dientich = chieucao * chieurong;
+        Console.WriteLine($"Gia tri dien tich la {dientich}" );
+    }
+}
---
+++ example 3/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        double bankinh;
+        Console.WriteLine("Hay nhap ban kinh hinh tron");
+        bankinh = Convert.ToDouble(Console.ReadLine());
+        double chuvi = Math.PI * bankinh * 2;
+        double dientich = Math.PI * bankinh * bankinh;
+        Console.WriteLine($"Chu vi hinh tron la {chuvi} ");
+        Console.WriteLine($"Dien tich hinh tron la {dientich}");
+    }
+}
