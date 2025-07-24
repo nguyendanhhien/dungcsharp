3356b137934374494829016e3a34fb19a90566ea
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 09:35:14 +0700

    add
    add

---
+++ examplemaxarray/Program.cs
@@ -0,0 +1,17 @@
+﻿﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] myarray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int max = myarray[0];
+        for (int i = 0; i < myarray.Length; i++)
+        {
+            if (myarray[i] > max)
+            {
+                max = myarray[i];
+            }
+           
+        }
+        Console.WriteLine($"Gia tu lon  nhat la {max}");
+    }
+}
