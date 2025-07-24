d47afabcd6543bc72ecb6c4421e4f1dc7e11c171
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 07:47:23 +0700

    add
    add

---
+++ exampletygia/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int tienviet;
+        int tiendola;
+        Console.WriteLine("Nhap so tien do la: ");
+        tiendola = Convert.ToInt32(Console.ReadLine());
+        tienviet = tiendola * 23000;
+        Console.WriteLine("So tien Viet Nam la: " + tienviet); 
+        
+
+    }
+}
