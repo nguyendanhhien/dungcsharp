bc2cd83846c932bbc5bc045fa330a0b00a0caa30
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 07:38:14 +0700

    add
    add

---
+++ examplechao/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hay nhap ten cua ban");
+        string name = Console.ReadLine();
+        Console.WriteLine("xin chao " + name);
+    }
+}
