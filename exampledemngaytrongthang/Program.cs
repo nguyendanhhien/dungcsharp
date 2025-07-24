fd65684a4b4311cf240ef4f43406e39e5729630a
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 08:04:03 +0700

    add
    add

---
+++ exampledemngaytrongthang/Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program 
+{
+    public static void Main(string[] args)
+    {
+        int thang;
+        Console.WriteLine("Nhap thang ban muon dem so ngay:");
+        thang = Convert.ToInt32(Console.ReadLine());
+        int soNgay;
+        switch (thang)
+        {
+            case 1: // January
+            case 3: // March
+            case 5: // May
+            case 7: // July
+            case 8: // August
+            case 10: // October
+            case 12: // December
+                soNgay = 31;
+                break;
+            case 4: // April
+            case 6: // June
+            case 9: // September
+            case 11: // November
+                soNgay = 30;
+                break;
+            case 2: // February
+                Console.WriteLine("Nhap nam:");
+                int nam = Convert.ToInt32(Console.ReadLine());
+                if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 400 == 0))
+                {
+                    soNgay = 29; // Leap year
+                }
+                else
+                {
+                    soNgay = 28; // Non-leap year
+                }
+                break;
+            default:
+                Console.WriteLine("Thang khong hop le.");
+                return;
+        }
+        Console.WriteLine($"Thang {thang} co {soNgay} ngay.");
+    }
+}
