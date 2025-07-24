856f9d82501c8c8d99a86b5892c4274e66dd9f81
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 07:58:56 +0700

    add
    add

---
+++ examplenamnhuan/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int nam;
+        bool namnhuan = false;
+        Console.Write("Nhap nam: ");
+        nam = Convert.ToInt32(Console.ReadLine());
+        if (nam % 4 == 0)
+        {
+            if (nam % 100 == 0)
+            {
+                if (nam % 400 == 0)
+                {
+                    namnhuan = true;
+                }
+            }
+            else
+            {
+                namnhuan = true;
+            }
+        }
+        if (namnhuan)
+        {
+            Console.WriteLine("Nam {0} la nam nhuan", nam);
+        }
+        else
+        {
+            Console.WriteLine("Nam {0} khong phai la nam nhuan", nam);
+        }
+        
+    }
+}
