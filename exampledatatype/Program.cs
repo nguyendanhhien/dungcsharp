9507346efecc01d28ca4dcbe8d399241bcc8061b
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 07:33:45 +0700

    add
    add

---
+++ exampledatatype/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        bool b = true;
+        char c = 'a';
+        String s = "CodeGym";
+        Console.WriteLine("Integer: " + i);
+        Console.WriteLine("Float: " + f);
+        Console.WriteLine("Double: " + d);
+        Console.WriteLine("Boolean: " + b);
+        Console.WriteLine("Character: " + c);
+        Console.WriteLine("String: " + s);
+    }
+}
