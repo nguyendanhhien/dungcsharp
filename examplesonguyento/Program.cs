dc769d50590876a6c48b937dd39b3fdcc4ccfc5b
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 09:07:36 +0700

    add
    add

---
+++ examplesonguyento/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int dem = 0;
+        Console.WriteLine("Danh sach cac so nguyen to tu 1 den 100 la:");
+        for (int i = 2; i <= 100; i++)
+        {
+            bool isPrime = true;
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine(i);
+                dem++;
+            }
+        }
+        Console.WriteLine("Tong so nguyen to tu 1 den 100 la: " + dem);
+    }
+}
