4e0644c7aad9f33c729316a3f8a568bd0d1ffee0
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-24 09:15:48 +0700

    add
    add

---
+++ examplekiemtrasonguyento/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int so = 0;
+        Console.WriteLine("Nhap mot so nguyen:");
+        so = Convert.ToInt32(Console.ReadLine());
+        if (so < 2)
+        {
+            Console.WriteLine("Khong phai la so nguyen to");
+        }
+        else
+        {
+            bool isPrime = true;
+            for (int i = 2; i <= Math.Sqrt(so); i++)
+            {
+                if (so % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("La so nguyen to");
+            }
+            else
+            {
+                Console.WriteLine("Khong phai la so nguyen to");
+            }
+        }
+    }
+}
