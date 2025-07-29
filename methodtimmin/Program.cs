37de64aa2e7ffe9bab6094368aa811884e7206fa
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-29 16:18:08 +0700

    add
    add

---
+++ methodtimmin/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 5, 3, 8, 1, 4 };
+        int index = MinValue(arr);
+        Console.WriteLine($"Minimum value is at index: {index}");
+        Console.WriteLine($"Minimum value is: {arr[index]}");
+
+    }
+    public static int MinValue(int[] array)
+    {
+        int min = array[0];
+        int index = 0;
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+                index = i;
+            }
+        }
+        return index;
+        
+    }
+}
