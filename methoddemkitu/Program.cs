bad97e8194277019372a5a9c282bd0a1441d0900
nguyendanhhien <nguyendanhhien2005@gmail.com>
2025-07-29 16:45:07 +0700

    add
    add

---
+++ methoddemkitu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hay nhap chuoi ban can dem so ki tu: ");
+        string input = Console.ReadLine();
+
+        Console.Write("Nhap ky tu can dem: ");
+        char inputChar = char.Parse(Console.ReadLine());
+
+        int count = CountCharacter(input, inputChar);
+        Console.WriteLine($"Ky tu '{inputChar}' xuat hien {count} lan.");
+    }
+
+    public static int CountCharacter(string input, char inputChar)
+    {
+        int count = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == inputChar)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
